23b362a12ec2dd5e6dff0558d0f3831d94a35a01
zhyyt <yangyuntaoyzm@gmail.com>
2025-08-26 14:51:11 +0800

    Update fixname.js

--- script/fixname.js
+++ script/fixname.js
@@ -4,20 +4,21 @@
 // 定义要修改的URL模式
 
 if (!$response.body) $done({});
-let body = $response.body;
-let modifiedBody = body.toString();
-modifiedBody = modifiedBody.replace(
+let body = $response.body.toString();
+
+body = body.replace(
    // <div class="item-identity">[\s\S]*?<dt>이름<\/dt>[\s\S]*?<dd>([\s\S]*?)<\/dd>
     //<div class="item-identity">\s*<dt>본인인증일<\/dt>\s*<dd>(\d{4}\.\d{2}\.\d{2})<\/dd>
     /2025.02.03/, 
     '2025.02.29'
   );
-$done({modifiedBody}); 
+$done({body}); 
  
 
 
  
   
+
 
 
 
