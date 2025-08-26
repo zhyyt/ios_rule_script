ee8577f1659c52ed35dc7a3944b038601e589611
zhyyt <yangyuntaoyzm@gmail.com>
2025-08-26 14:30:17 +0800

    Update fixname.js

--- script/fixname.js
+++ script/fixname.js
@@ -5,31 +5,20 @@
 
 if (!$response.body) $done({});
 let body = $response.body;
-
-function modifyResponse(body) {
-  let modifiedBody = body;
-  
-  // 修改页面标题
-  modifiedBody = modifiedBody.replace(
+let modifiedBody = body.toString();
+modifiedBody = modifiedBody.replace(
    // <div class="item-identity">[\s\S]*?<dt>이름<\/dt>[\s\S]*?<dd>([\s\S]*?)<\/dd>
     //<div class="item-identity">\s*<dt>본인인증일<\/dt>\s*<dd>(\d{4}\.\d{2}\.\d{2})<\/dd>
     /2025.02.03/, 
-    '2025.02.29>'
+    '2025.02.29'
   );
-    
-  return modifiedBody;
-}
+$done({modifiedBody}); 
+ 
 
- // 如果是HTML内容，则进行修改
-    if ($response.headers['Content-Type'] && 
-        $response.headers['Content-Type'].includes('text/html')) {
-      body = modifyResponse(body.toString());
-      
-    }
-     $done({body});
-     else {
 
-    $done({});
+ 
+  
+
 
 
 
